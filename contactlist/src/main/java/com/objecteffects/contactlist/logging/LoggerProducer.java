bb4f34a3e3122ddd5f336743d2c387f11ca7fe76
Rusty <rusty.wright@gmail.com>
2023-09-22 18:06:26 -0700

    push 002

--- contactlist/src/main/java/com/objecteffects/contactlist/logging/LoggerProducer.java
+++ contactlist/src/main/java/com/objecteffects/contactlist/logging/LoggerProducer.java
@@ -11,7 +11,6 @@ import jakarta.inject.Singleton;
 @Model
 @Singleton
 public class LoggerProducer {
-
     @Produces
     public Logger produceLogger(final InjectionPoint injectionPoint) {
         return LoggerFactory
