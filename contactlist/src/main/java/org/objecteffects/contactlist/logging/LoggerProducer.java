56138d53374dbbb3a23cd58a6f22760fd0b411ae
Rusty <rusty.wright@gmail.com>
2023-09-30 11:35:19 -0700

    changed jpa code to use criteria queries, for "fun".  fixed persistence.xml and web.xml.

--- contactlist/src/main/java/org/objecteffects/contactlist/logging/LoggerProducer.java
+++ contactlist/src/main/java/org/objecteffects/contactlist/logging/LoggerProducer.java
@@ -1,19 +1,41 @@
 package org.objecteffects.contactlist.logging;
 
+import java.io.Serializable;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import jakarta.enterprise.inject.Model;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.enterprise.inject.spi.InjectionPoint;
-import jakarta.inject.Singleton;
 
-@Model
-@Singleton
-public class LoggerProducer {
+//@Model
+//@Singleton
+//public class LoggerProducer {
+//    @Produces
+//    public Logger produceLogger(final InjectionPoint injectionPoint) {
+//        return LoggerFactory
+//                .getLogger(injectionPoint.getMember().getDeclaringClass());
+//    }
+//}
+
+@ApplicationScoped
+public class LoggerProducer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     @Produces
     public Logger produceLogger(final InjectionPoint injectionPoint) {
-        return LoggerFactory
-                .getLogger(injectionPoint.getMember().getDeclaringClass());
+        final Class<?> loggerName = extractLoggerName(injectionPoint);
+
+        return LoggerFactory.getLogger(loggerName);
+    }
+
+    private Class<?> extractLoggerName(final InjectionPoint injectionPoint) {
+        if (injectionPoint.getBean() == null) {
+            return injectionPoint.getMember().getDeclaringClass();
+        }
+
+        return injectionPoint.getBean().getBeanClass();
     }
 }
--- contactlist/src/main/java/org/objecteffects/contactlist/service/ContactService.java
+++ contactlist/src/main/java/org/objecteffects/contactlist/service/ContactService.java
@@ -10,6 +10,10 @@ import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaDelete;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 @Stateless
 public class ContactService implements Serializable {
@@ -24,28 +28,58 @@ public class ContactService implements Serializable {
         this.entityManager.persist(contact);
     }
 
-    public List<Contact> getContacts() {
-        this.log.debug("get contacts");
+//    public List<Contact> getContacts() {
+//        this.log.debug("get contacts");
+//
+//        return this.entityManager.createQuery("select c from Contact c",
+//                Contact.class).getResultList();
+//    }
 
-        return this.entityManager.createQuery("select c from Contact c",
-                Contact.class).getResultList();
+    public List<Contact> getContacts() {
+        final CriteriaQuery<Contact> cq =
+                this.entityManager.getCriteriaBuilder()
+                        .createQuery(Contact.class);
+
+        cq.select(cq.from(Contact.class));
+
+        return this.entityManager.createQuery(cq).getResultList();
     }
 
     public Contact getContact(final Long id) {
         this.log.debug("get contact, {}", id);
 
-        return this.entityManager.find(Contact.class, id);
+        final CriteriaBuilder cb = this.entityManager.getCriteriaBuilder();
+
+        final CriteriaQuery<Contact> cq = cb.createQuery(Contact.class);
+
+        final Root<Contact> contact = cq.from(Contact.class);
+
+        cq.select(contact);
+        cq.distinct(true);
+        cq.where(cb.equal(contact.get("id"), id));
+
+        return this.entityManager.createQuery(cq).getSingleResult();
     }
 
     public void deleteContact(final Long id) {
         this.log.debug("delete contact, {}", id);
 
-        final Contact contact = this.entityManager.find(Contact.class, id);
+        final CriteriaBuilder cb = this.entityManager.getCriteriaBuilder();
 
-        if (contact != null) {
-            this.log.debug("removing {} {}", contact.getFirstName(),
-                    contact.getLastName());
-            this.entityManager.remove(contact);
-        }
+        // create delete
+        final CriteriaDelete<Contact> cq =
+                cb.createCriteriaDelete(Contact.class);
+
+        // set the root class
+        final Root<Contact> contact = cq.from(Contact.class);
+
+        // set where clause
+        cq.where(cb.equal(contact.get("id"), id));
+
+        // perform update
+        final int deletes = this.entityManager.createQuery(cq).executeUpdate();
+
+        this.log.debug("deleted {}", Long.valueOf(deletes));
+//        }
     }
 }
